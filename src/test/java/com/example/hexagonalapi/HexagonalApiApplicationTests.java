80f8b0c8130c30d9b2917d8ba5c05aac76f51751
gustavobackes <gustavo.kitzberger@projuris.com.br>
2025-02-07 17:37:22 -0300

    Primeiro commit

---
+++ src/test/java/com/example/hexagonalapi/HexagonalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexagonalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
